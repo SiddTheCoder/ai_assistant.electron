f1a9510ca933252eb0e450505c2e66447045e6d9
SiddTheCoder <siddthecoder@gmail.com>
2025-11-14 19:09:08 +0545

    feat: add Home component to the renderer pages

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Lander from './pages/Lander'
+import Home from './pages/Home'
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Lander />} />
+        <Route path="/home" element={<Home />} />
     </Routes>
     </BrowserRouter>
   )
--- src/renderer/pages/Lander.tsx
+++ src/renderer/pages/Lander.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import { useState, useEffect, useMemo } from "react";
 // @ts-ignore
-import icon from "../../assets/text-icon.png";
-import "../App.css"
+import TextIcon from "../../assets/text-icon.png";
+import "../App.css";
+// @ts-ignore
+import BackgroundImage from "../../assets/bg-paper-icon.jpg";
 
 export default function Lander() {
   return (
-    <div className='h-screen w-screen webkit-drag-drag flex items-center justify-center'>
-      <img src={icon} alt="ICON" width={600} />
+    <div
+      className="h-screen w-screen webkit-drag-drag flex items-center justify-center relative"
+      style={{
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <img src={TextIcon} alt="ICON" width={600} />
+      <span className="absolute bottom-10">Version 1.0</span>
     </div>
-  )
+  );
 }
